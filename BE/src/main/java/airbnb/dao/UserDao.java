fd7908f1bbef730eb4bb09dc3443ab3f639fbb7a
95degree <95degree@naver.com>
2021-06-02 17:10:53 +0900

    feat: ✨ User 도메인을 가져오는 쿼리문 생성(#43)

--- BE/src/main/java/airbnb/dao/UserDao.java
+++ BE/src/main/java/airbnb/dao/UserDao.java
@@ -1,4 +1,27 @@
 package airbnb.dao;
 
+import airbnb.domain.User;
+import airbnb.mapper.UserMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final UserMapper userMapper;
+    private final MapSqlParameterSource parameter = new MapSqlParameterSource();
+
+    public UserDao(NamedParameterJdbcTemplate jdbcTemplate, UserMapper userMapper) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.userMapper = userMapper;
+    }
+
+    public User findById(String userId){
+        String sql = "SELECT user.id, image.url FROM user JOIN image ON user.id = image.user_id " +
+                "WHERE user.id = :userId";
+        parameter.addValue("userId", userId);
+        return jdbcTemplate.queryForObject(sql,parameter,userMapper);
+    }
 }
--- BE/src/main/java/airbnb/mapper/UserMapper.java
+++ BE/src/main/java/airbnb/mapper/UserMapper.java
@@ -2,10 +2,12 @@ package airbnb.mapper;
 
 import airbnb.domain.User;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class UserMapper implements RowMapper<User> {
 
     @Override
